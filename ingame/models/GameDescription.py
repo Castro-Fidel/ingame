e96ebca97c7064263f0031f4c9f7e57c7e5b911f
Yankovskiy Georgiy <admin@yageorgiy.ru>
2024-06-28 02:15:43 +0300

    Commits 918563db, a19804a8 refactor (part 1 of 2)

---
+++ ingame/models/GameDescription.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class GameDescription:
+    title: str = 'Информация не найдена!'
+    desc: str = 'Информация не найдена!'
+    languages: str = 'Информация не найдена!'
+    reqs: str = 'Информация не найдена!'
+    image_location_path: str = ''
+
+    def as_dict(self):
+        formatted_data = dict()
+        formatted_data['title'] = self.title
+        formatted_data['desc'] = self.desc
+        formatted_data['languages'] = self.languages
+        formatted_data['reqs'] = self.reqs
+        formatted_data['image_location_path'] = self.image_location_path
+        return formatted_data
--- ingame/models/GamesModel.py
+++ ingame/models/GamesModel.py
@@ -1,45 +1,40 @@
 import typing
-from dataclasses import dataclass, fields
+from dataclasses import fields
 from PySide6.QtCore import QAbstractListModel, QModelIndex, Qt, QByteArray
 
-
-@dataclass
-class Game:
-    name: str = ''
-    exec: str = ''
-    icon: str = ''
+from ingame.models.GameEntry import GameEntry
 
 
 class GamesModel(QAbstractListModel):
 
     def __init__(self):
         super().__init__()
-        self._list = []
+        self.games_list = []
 
     def data(self, index: QModelIndex, role: int = Qt.DisplayRole) -> typing.Any:
         if 0 <= index.row() < self.rowCount():
-            student = self._list[index.row()]
+            student = self.games_list[index.row()]
             name = self.roleNames().get(role)
             if name:
                 return getattr(student, name.decode())
 
     def roleNames(self) -> dict[int, QByteArray]:
         d = {}
-        for i, field in enumerate(fields(Game)):
+        for i, field in enumerate(fields(GameEntry)):
             d[Qt.DisplayRole + i] = field.name.encode()
         return d
 
     def rowCount(self, index: QModelIndex = QModelIndex()) -> int:
-        return len(self._list)
+        return len(self.games_list)
 
-    def add_game(self, game: Game) -> None:
+    def add_game(self, game: GameEntry) -> None:
         self.beginInsertRows(QModelIndex(), self.rowCount(), self.rowCount())
-        self._list.append(game)
+        self.games_list.append(game)
         self.endInsertRows()
 
     def clear(self) -> None:
         self.beginInsertRows(QModelIndex(), self.rowCount(), self.rowCount())
-        self._list = []
+        self.games_list = []
         self.endInsertRows()
 
     pass
