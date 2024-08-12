87d834462e23ca941f1d1cbc9a5245a8b14023e0
Llein <43903165+Eljeyna@users.noreply.github.com>
2024-08-12 20:41:52 +0300

    Fix controls and revert parameters

--- ingame/models/Gamepad.py
+++ ingame/models/Gamepad.py
@@ -21,6 +21,22 @@ class Gamepad:
         self.joystick: Union[Joystick, None] = None
 
         self.terminated: bool = False
+        self.last_rb_clicked: bool = False
+        self.last_lb_clicked: bool = False
+
+        self.last_left_clicked: bool = False
+        self.last_right_clicked: bool = False
+        self.last_up_clicked: bool = False
+        self.last_down_clicked: bool = False
+
+        self.last_dpad_left_clicked: bool = False
+        self.last_dpad_right_clicked: bool = False
+        self.last_dpad_up_clicked: bool = False
+        self.last_dpad_down_clicked: bool = False
+
+        self.last_apply_clicked: bool = False
+        self.last_back_clicked: bool = False
+
         self.lb_clicked: () = lambda: None
         self.rb_clicked: () = lambda: None
         self.l_clicked: () = lambda: None
@@ -54,49 +70,83 @@ class Gamepad:
                         # joystick, filling up the list without needing to create them manually.
                         self.joystick = pygame.joystick.Joystick(event.device_index)
                         joysticks[self.joystick.get_instance_id()] = self.joystick
-                        print(f"Joystick {self.joystick.get_instance_id()} connected")
 
                     if event.type == pygame.JOYDEVICEREMOVED:
                         del joysticks[event.instance_id]
                         for joy in joysticks:
                             if joy != None:
                                 self.joystick = joysticks[joy]
-                                print(f"Joystick {self.joystick.get_instance_id()} is used")
                                 break
-                        print(f"Joystick {event.instance_id} disconnected")
 
                     if event.type == pygame.JOYBUTTONDOWN:
                         self.joystick = joysticks[self.joystick.get_instance_id()]
 
                         # LB
-                        if event.button == self.LB_BUTTON:
+                        if event.button == self.LB_BUTTON and not self.last_lb_clicked:
+                            self.last_lb_clicked = not self.last_lb_clicked
                             self.lb_clicked()
                         # RB
-                        if event.button == self.RB_BUTTON:
+                        if event.button == self.RB_BUTTON and not self.last_rb_clicked:
+                            self.last_rb_clicked = not self.last_rb_clicked
                             self.rb_clicked()
                         # APPLY
-                        if event.button == self.APPLY_BUTTON:
+                        if event.button == self.APPLY_BUTTON and not self.last_apply_clicked:
+                            self.last_apply_clicked = not self.last_apply_clicked
                             self.apply_clicked()
                         # BACK
-                        if event.button == self.BACK_BUTTON:
+                        if event.button == self.BACK_BUTTON and not self.last_back_clicked:
+                            self.last_back_clicked = not self.last_back_clicked
                             self.back_clicked()
+
+                    if event.type == pygame.JOYBUTTONUP:
+                        # LB
+                        if event.button == self.LB_BUTTON and self.last_lb_clicked:
+                            self.last_lb_clicked = not self.last_lb_clicked
+                        # RB
+                        if event.button == self.RB_BUTTON and not self.last_rb_clicked:
+                            self.last_rb_clicked = not self.last_rb_clicked
+                        # APPLY
+                        if event.button == self.APPLY_BUTTON and not self.last_apply_clicked:
+                            self.last_apply_clicked = not self.last_apply_clicked
+                        # BACK
+                        if event.button == self.BACK_BUTTON and not self.last_back_clicked:
+                            self.last_back_clicked = not self.last_back_clicked
 
                     if event.type == pygame.JOYAXISMOTION:
                         self.joystick = joysticks[event.instance_id]
                         left_right_axis = self.joystick.get_axis(self.LEFT_RIGHT_AXIS)
                         up_down_axis = self.joystick.get_axis(self.UP_DOWN_AXIS)
                         # LEFT
-                        if left_right_axis <= -self.LEFT_RIGHT_AXIS_SENSITIVITY:
+                        if (left_right_axis <= -self.LEFT_RIGHT_AXIS_SENSITIVITY) and not self.last_left_clicked:
+                            self.last_left_clicked = not self.last_left_clicked
                             self.l_clicked()
+
+                        if not (left_right_axis <= -self.LEFT_RIGHT_AXIS_SENSITIVITY) and self.last_left_clicked:
+                            self.last_left_clicked = not self.last_left_clicked
+
                         # RIGHT
-                        if left_right_axis >= self.LEFT_RIGHT_AXIS_SENSITIVITY:
+                        if (left_right_axis >= self.LEFT_RIGHT_AXIS_SENSITIVITY) and not self.last_right_clicked:
+                            self.last_right_clicked = not self.last_right_clicked
                             self.r_clicked()
+
+                        if (not left_right_axis >= self.LEFT_RIGHT_AXIS_SENSITIVITY) and self.last_right_clicked:
+                            self.last_right_clicked = not self.last_right_clicked
+
                         # UP
-                        if up_down_axis <= -self.UP_DOWN_AXIS_SENSITIVITY:
+                        if (up_down_axis <= -self.UP_DOWN_AXIS_SENSITIVITY) and not self.last_up_clicked:
+                            self.last_up_clicked = not self.last_up_clicked
                             self.u_clicked()
+
+                        if not (up_down_axis <= -self.UP_DOWN_AXIS_SENSITIVITY) and self.last_up_clicked:
+                            self.last_up_clicked = not self.last_up_clicked
+
                         # DOWN
-                        if up_down_axis >= self.UP_DOWN_AXIS_SENSITIVITY:
+                        if (up_down_axis >= self.UP_DOWN_AXIS_SENSITIVITY) and not self.last_down_clicked:
+                            self.last_down_clicked = not self.last_down_clicked
                             self.d_clicked()
+
+                        if (not up_down_axis >= self.UP_DOWN_AXIS_SENSITIVITY) and self.last_down_clicked:
+                            self.last_down_clicked = not self.last_down_clicked
 
                     # DPAD support
                     if event.type == pygame.JOYHATMOTION:
@@ -104,18 +154,34 @@ class Gamepad:
                         for i in event.value:
                             hat[i] = event.value[i]
 
+                        if (hat[0] == 0 and hat[1] == 0):
+                            self.last_dpad_left_clicked = False
+                            self.last_dpad_right_clicked = False
+                            self.last_dpad_up_clicked = False
+                            self.last_dpad_down_clicked = False
+                            continue
+
                         # LEFT
-                        if (hat[0] == -1 and hat[1] == 0):
+                        if (hat[0] == -1 and hat[1] == 0 and not self.last_dpad_left_clicked):
+                            self.last_dpad_left_clicked = not self.last_dpad_left_clicked
                             self.l_clicked()
+
                         # RIGHT
-                        if (hat[0] == 1 and hat[1] == 0):
+                        if (hat[0] == 1 and hat[1] == 0 and not self.last_dpad_right_clicked):
+                            self.last_dpad_right_clicked = not self.last_dpad_right_clicked
                             self.r_clicked()
+
                         # UP
-                        if (hat[0] == 0 and hat[1] == 1):
+                        if (hat[0] == 0 and hat[1] == 1 and not self.last_dpad_up_clicked):
+                            self.last_dpad_up_clicked = not self.last_dpad_up_clicked
                             self.u_clicked()
+
                         # DOWN
-                        if (hat[0] == 0 and hat[1] == -1):
+                        if (hat[0] == 0 and hat[1] == -1 and not self.last_dpad_down_clicked):
+                            self.last_dpad_down_clicked = not self.last_dpad_down_clicked
                             self.d_clicked()
+
+                        hat = [0, 0]
 
         except pygame.error as e:
             print(e)
